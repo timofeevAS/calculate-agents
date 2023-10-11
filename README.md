e79f5d42b9bb954b6a569ac6cba16c89c044962f
Александр <timofeev2.as@edu.spbstu.ru>
2023-10-11 23:41:19 +0300

    Methods for Agent Model, README edit, Requirements update

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # calculate-agents
+This project implements some clusters model for calculate task. Main point is design logic of agents communication.
+
+
+## Dependecies
+
+* Python 3.11
+* FastAPI >= 0.103.x
+
+## Build
+
+```bash
+git clone https://github.com/timofeevAS/calculate-agents
+cd ./calculate-agents
+python -m venv venv
+cd ./.venv/Scripts
+./activate
+cd ../..
+```
+
+## Run
+
+```bash
+uvicorn main:app --reload    
+```
+
+Your application can available [127.0.0.1:8000](http://127.0.0.1:8000/docs)
--- main.py
+++ main.py
@@ -1,8 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import json
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+class Agent(BaseModel):
+    """
+    Model of Agent
+    """
+    task: str | None = None
+    name: str | None = None
+    is_busy: bool | None = None
+    is_manager: bool | None = None
+
+
+def save_agents_to_file(agents):
+    with open('agents.json', 'w') as f:
+        json.dump(agents, f)
+
+
+def load_agents_from_file(filename='agents.json'):
+    try:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        data = []
+    return data
+
+
+@app.post("/agents/")
+async def create_agent(agent: Agent, request: Request):
+    """
+    Method to create agent
+    :param agent:
+    :param request:
+    :return:
+    """
+    # Filling data in new Agent
+    host, port = request.client.host, request.client.port
+    agent.name = f"{host}:{port}"
+    agent.is_manager = False
+
+    agents = load_agents_from_file()  # All agents from file
+    agents.append(agent.model_dump())  # Append new agent
+    print(agent)
+    save_agents_to_file(agents)  # Saving new agent list
+
+    print(agent.model_dump())
+    return agent
+
+
+@app.get("/agents/")
+async def get_agents():
+    """
+    Method to create agent
+    :param agent:
+    :param request:
+    :return:
+    """
+    agents = load_agents_from_file()
+    return agents
