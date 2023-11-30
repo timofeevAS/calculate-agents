733e73fcc2bd9e0da57ae4d28611811de45ede2e
timofeevAS <a.timoff3@gmail.com>
2023-12-01 00:19:44 +0300

    [0] function to find ports

---
+++ utils.py
@@ -0,0 +1,22 @@
+import socket
+
+def find_free_port(start_port, end_port):
+    """
+    Find a free port within the specified range.
+
+    Parameters:
+    - start_port (int): The start of the port range.
+    - end_port (int): The end of the port range.
+
+    Returns:
+    - int: The first available port within the range, or None if no port is available.
+    """
+    for port in range(start_port, end_port + 1):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex(('localhost', port))
+        sock.close()
+        if result != 0:
+            return port
+
+    return -1
