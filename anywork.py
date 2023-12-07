fa68ebaf8f53b6806aa4b57680da24f1de770f0e
KevGen <116305013+KevGen6908@users.noreply.github.com>
2023-12-07 13:41:20 +0300

    Update anywork.py

--- anywork.py
+++ anywork.py
@@ -10,38 +10,6 @@ from agent import args, give_task_to_worker
 from models import Agent
 from main import agents_db, tasks_db
 
-app = FastAPI()
-
-
-# async def send_request():
-#     while True:
-#         free_workers = []
-#         for agent in agents_db.get_all_records():
-#             if agent['role'] == 'worker' and agent['state'] == 'ready':
-#                 free_workers.append(agent)
-#
-#         if free_workers:
-#             tasks = tasks_db.get_all_records()
-#             tasks_args = []
-#
-#             for index, workers in enumerate(free_workers):
-#                 if index >= len(tasks):
-#                     break
-#                 print(f"Giving task_{index} for worker {workers}")
-#                 tasks_args.append((workers, tasks[index]))
-#             print(tasks_args)
-#             await asyncio.gather(*(give_task_to_worker(*args) for args in tasks_args))
-#         await asyncio.sleep(30)
-#
-# @app.post("/manager/ping")
-# async def check_free_workers_and_ping():
-#     asyncio.create_task(check_free_workers_and_ping())
-#     return JSONResponse(content={"message": "Check for free workers scheduled."},
-#                         status_code=status.HTTP_200_OK)
-
-jsonData = JsonDB
-
-
 def get_manager_address():
     with open('agents.json', 'r') as file:
         agent_data = json.load(file)
